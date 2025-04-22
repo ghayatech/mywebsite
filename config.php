afb8994c4c6304a7b6917ccf1b2b20e4fedce48a
AboFhme <mohammedabofhme74@gmail.com>
2025-04-22 13:15:36 +0300

    Remove config from repository

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mail_host = 'smtp.gmail.com';
-$mail_username = 'ghayatech.media@gmail.com';       // بريدك في Gmail
-$mail_password = 'rgmx xvez pavh libr';           // الـ App Password من الخطوة السابقة
-$mail_port = 587;
